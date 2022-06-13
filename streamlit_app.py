688783b5db0b198a65453bc90020d009e2c3299b
athirah4 <athira.haridas94@gmail.com>
2022-06-13 13:23:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinnach & Rocket smoothie')
+streamlit.text('Hard-boiled Free-Range Eggs')
+
